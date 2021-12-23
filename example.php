b8eba6d073bf89b198c8553a3ec192848a7f209c
aboodma58 <34406204+aboodma@users.noreply.github.com>
2021-12-23 07:01:50 +0300

    Reformat Files

---
+++ example.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * BTC API CLASS
+ */
--- src/index.php
+++ src/index.php
@@ -2,10 +2,26 @@
 
 namespace aboodma\BtcTurk;
 
-class Index
+class BTCTURK
 {
-    public function greet($greet = "Hello World")
-    {
-        return $greet;
-    }
+  protected  $ApiKey , $PrivateKey;
+  protected  $BaseURI = "https://api.btcturk.com";
+  function __construct(String $_ApiKey,String $_PrivateKey)
+  {
+  $this->ApiKey = $_ApiKey;
+  $this->PrivateKey = $_PrivateKey;
+  }
+
+  private function GetPrivateKey(){
+    return $this->PrivateKey;
+  }
+
+  private function GetApiKey(){
+    return $this->ApiKey;
+  }
+public function test()
+{
+  // code...
+}
+
 }
